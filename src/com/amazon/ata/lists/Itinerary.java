2a4fa80292ea0f7cc73b9c520006e35cf8c9ae4a
Santiago Coria <santiago_coria@hotmail.com>
2024-04-06 16:55:05 -0300

    ArrayList methods

--- src/com/amazon/ata/lists/Itinerary.java
+++ src/com/amazon/ata/lists/Itinerary.java
@@ -3,6 +3,7 @@ package com.amazon.ata.lists;
 import com.amazon.ata.resources.lists.prework.Destination;
 
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Class representing travel itinerary. The basic functionality it supports is to add, get or remove
@@ -10,12 +11,17 @@ import java.util.List;
  * and get the total number of days that will be spent in trip.
  */
 public class Itinerary {
+    private List<Destination> destinations = new ArrayList<Destination>();
+    private List<String> locations = new ArrayList<String>();
+
     /**
      * Add a new destination to the end of itinerary.
      *
      * @param destination destination to add to itinerary
      */
     public void addDestination(Destination destination) {
+        destinations.add(destination);
+        locations.add(destination.getLocation());
         // Implement the method here
     }
 
@@ -26,10 +32,14 @@ public class Itinerary {
      * @return Destination at position.
      */
     public Destination getDestination(int position) {
-        // Implement the method here
-        return null;
-    }
+        if (position >= 0 && position < destinations.size()) {
+            return destinations.get(position);
+        } else {
 
+            // Implement the method here
+            return null;
+        }
+    }
     /**
      * Remove a destination from itinerary based on its position.
      *
@@ -37,10 +47,15 @@ public class Itinerary {
      * @return destination that was removed from itinerary.
      */
     public Destination removeDestination(int position) {
-        // Implement the method here
-        return null;
-    }
-
+        if (position >= 0 && position < destinations.size()) {
+            Destination removed = destinations.remove(position);
+            if (removed != null) {
+                locations.remove(position);
+            }
+            return removed;
+        } else {
+            throw new IndexOutOfBoundsException("Invalid position or empty list");
+        }}
 
     /**
      * Get list of locations (cities) in the order in which they
@@ -49,8 +64,10 @@ public class Itinerary {
      * @return full list of locations from itinerary.
      */
     public List<String> getListOfLocations() {
+        return locations;
+
         // Implement the method here
-        return null;
+  /*   return null;*/
     }
 
     /**
@@ -60,8 +77,13 @@ public class Itinerary {
      * @return total number of days spent in all locations.
      */
     public int getTotalNumberOfDays() {
-        // Implement the method here
-        return -1;
+        int totalDays = 0;
+        for (Destination destination : destinations) {
+            totalDays += destination.getDaysAtLocation();
+        }
+        return totalDays;
+
+   /*     return -1;*/
     }
 
     /**
@@ -70,7 +92,8 @@ public class Itinerary {
      * @return number of destinations contained in itinerary.
      */
     public int getNumberOfDestinations() {
-        // Implement the method here
-        return -1;
+        return destinations.size();
+
+    /*   return -1;*/
     }
 }
